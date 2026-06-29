e8fc0deeb90ff5b965222f176e8658c06bd43ecc
auto-smart-home <2183215@gmail.com>
2026-06-29 09:03:04 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,10 +8,13 @@ MQTT_USER=$(jq -r '.mqtt_user // ""' $OPTIONS)
 MQTT_PASS=$(jq -r '.mqtt_pass // ""' $OPTIONS)
 YEMOT_API_TOKEN=$(jq -r '.yemot_api_token // ""' $OPTIONS)
 YEMOT_API_LINK_URL=$(jq -r '.yemot_api_link_url // ""' $OPTIONS)
-ADMIN_PASSWORD=$(jq -r '.admin_password // "changeme"' $OPTIONS)
+ADMIN_PASSWORD=$(jq -r '.admin_password // ""' $OPTIONS)
 CONTROLLERS=$(jq -c '.controllers // []' $OPTIONS)
 
-export CONFIG_JSON="{\"MQTT_URL\":\"${MQTT_URL}\",\"MQTT_USER\":\"${MQTT_USER}\",\"MQTT_PASS\":\"${MQTT_PASS}\",\"YEMOT_PHONE_MAP\":{},\"CONTROLLERS\":${CONTROLLERS},\"USERS\":[{\"name\":\"admin\",\"password\":\"${ADMIN_PASSWORD}\",\"role\":\"admin\",\"relays\":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]}],\"EMERGENCY_PASSWORD\":null}"
+# ADMIN_PASSWORD מועבר כ-EMERGENCY_PASSWORD —
+# כך הכניסה עובדת גם לפני שמשתמשים הוגדרו במימשק,
+# ועם כל שם משתמש (הקוד מחפש admin ואם לא נמצא לוקח ראשון)
+export CONFIG_JSON="{\"MQTT_URL\":\"${MQTT_URL}\",\"MQTT_USER\":\"${MQTT_USER}\",\"MQTT_PASS\":\"${MQTT_PASS}\",\"YEMOT_PHONE_MAP\":{},\"CONTROLLERS\":${CONTROLLERS},\"USERS\":[{\"name\":\"admin\",\"password\":\"placeholder_will_be_overridden\",\"role\":\"admin\",\"relays\":[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]}],\"EMERGENCY_PASSWORD\":\"${ADMIN_PASSWORD}\"}"
 
 export YEMOT_API_TOKEN
 export YEMOT_API_LINK_URL
